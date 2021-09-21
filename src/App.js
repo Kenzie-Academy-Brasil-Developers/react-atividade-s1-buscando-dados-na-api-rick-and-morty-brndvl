e9796c414c6d9c6216c9a9c488d879fa7de5afaf
Bruno Dela Val Assis <bc.delaval@gmail.com>
2021-09-21 03:36:44 -0300

    adding buttons and new request

--- src/App.js
+++ src/App.js
@@ -6,20 +6,38 @@ function App() {
 
   const [characterList, setcharacterList] = useState([])
 
+  const [next, setNext] = useState(1)
+
   useEffect(() => {
-    fetch("https://rickandmortyapi.com/api/character/")
+    fetch(`https://rickandmortyapi.com/api/character?page=${next}`)
     .then((response) => response.json())
     .then((response) => setcharacterList(response.results))
     .catch((err) => console.log(err))
-  }, [])
+  }, [next])
 
   console.log(characterList)
+
+  const previousPage = () => {
+    if(next > 1) {
+    setNext(next - 1)
+    }
+  }
+
+  const nextPage = () => {
+    setNext(next + 1)
+
+  }
 
   return (
     <div className="App">
       <div className="App-header">
       <h2>Meus Personagens</h2>
         <Characters characterList={characterList} />
+      </div>
+
+      <div className="buttons">
+        <button onClick={previousPage}>Voltar</button>
+        <button onClick={nextPage}>Avançar</button>
       </div>
     </div>
   );
